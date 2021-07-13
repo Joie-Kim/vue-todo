3f024c241af6fbe8213eeca2480188dc68f1315a
JoieHuiju <joie.huiju@gmail.com>
2021-07-13 11:13:21 +0900

    [Add] Set index.html and Add components
    [Set index.html]
      - 반응형 웹을 위해 viewport 사용
      - Icon 사용을 위해 fontawesome 사용
      - 구글 폰트 사용
    [Add components]
      - header, input, list, footer 추가

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">
+      Add
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function() {
+      // this는 해당 컴포넌트를 가리킴
+      // console.log(this.newTodoItem);
+
+      // 저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+
+      // 저장한 뒤에 빈 문자열로 초기화
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style></style>
