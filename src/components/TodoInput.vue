01c5669412c1d0b859c2be6b87c2d1adb83fb63f
JoieHuiju <joie.huiju@gmail.com>
2021-07-13 18:17:58 +0900

    [Update] Add Modal component and Transition
    [Modal]
    - input 값이 없는데, Add 버튼 누른 경우 모달창이 나와 안내하도록 추가
    [Transition]
    - todo 리스트에 항목 추가 및 삭제 시, 효과 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,25 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        Warn!
+        <i class="fas fa-times closeModalBtn" @click="showModal = false"></i>
+      </h3>
+      <p slot="body">내용을 입력하세요.</p>
+      <!-- <button slot="footer">OK</button> -->
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
   data: function() {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
@@ -19,11 +30,16 @@ export default {
       if (this.newTodoItem !== "") {
         this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function() {
       this.newTodoItem = "";
     },
+  },
+  components: {
+    Modal: Modal,
   },
 };
 </script>
@@ -53,5 +69,8 @@ input:focus {
   color: white;
   vertical-align: middle;
   cursor: pointer;
+}
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
